9a2610e5a3bb880abd0d41af30e2bafaa0bff3a2
Vahid Zafari <v.zafari@chmail.ir>
2020-04-16 17:50:59 +0430

    disable some spring boot's auto configurations

--- src/main/java/org/bardframework/crud/sample/Application.java
+++ src/main/java/org/bardframework/crud/sample/Application.java
@@ -6,7 +6,11 @@ import org.bardframework.commons.web.filter.ExceptionHandlerFilter;
 import org.bardframework.crud.sample.config.CrudSampleConfiguration;
 import org.bardframework.crud.sample.config.CrudSampleExceptionControllerAdvice;
 import org.bardframework.crud.sample.config.JacksonConfiguration;
+import org.bardframework.crud.sample.config.JdbcConfig;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -15,10 +19,10 @@ import org.springframework.core.Ordered;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class, ValidationAutoConfiguration.class})
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class, ValidationAutoConfiguration.class, JacksonAutoConfiguration.class, DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
 @ComponentScan(basePackages = {"org.bardframework.crud.sample"}, excludeFilters = {@ComponentScan.Filter(value = {Configuration.class})})
 @EnableAspectJAutoProxy(proxyTargetClass = true)
-@Import({JacksonConfiguration.class, CrudSampleConfiguration.class, CrudSampleExceptionControllerAdvice.class})
+@Import({JacksonConfiguration.class, CrudSampleConfiguration.class, CrudSampleExceptionControllerAdvice.class, JdbcConfig.class})
 @ImportResource("classpath*:spring-configuration/**/**.xml")
 @EnableWebMvc
 @EnableTransactionManagement
