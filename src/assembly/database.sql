6b836562f946deb8f8f8d7802e1d6eb19fefa947
Vahid Zafari <va.zafari@gmail.com>
2022-08-10 08:02:03 +0430

    clean code

--- src/assembly/database.sql
+++ src/assembly/database.sql
@@ -1,16 +1,38 @@
-CREATE DATABASE IF NOT EXISTS `crud-sample` CHAR SET utf8 COLLATE utf8_bin;
+CREATE
+DATABASE IF NOT EXISTS `crud-sample` CHAR SET utf8 COLLATE utf8_bin;
 CREATE TABLE IF NOT EXISTS `crud-sample`.`tb_employee`
 (
-    `id`          char(50)  NOT NULL,
-    `email`       char(100) NOT NULL,
-    `first_name`  varchar(100) DEFAULT NULL,
-    `last_name`   varchar(100) DEFAULT NULL,
-    `description` varchar(500) DEFAULT NULL
-);
+    `id` char
+(
+    50
+) NOT NULL,
+    `email` char
+(
+    100
+) NOT NULL,
+    `first_name` varchar
+(
+    100
+) DEFAULT NULL,
+    `last_name` varchar
+(
+    100
+) DEFAULT NULL,
+    `description` varchar
+(
+    500
+) DEFAULT NULL
+    );
 
 CREATE TABLE IF NOT EXISTS `crud-sample`.`tb_gender`
 (
-    `id`    char(50)  NOT NULL,
-    `title` char(100) NOT NULL,
-    `order` int       NOT NULL
-);
+    `id` char
+(
+    50
+) NOT NULL,
+    `title` char
+(
+    100
+) NOT NULL,
+    `order` int NOT NULL
+    );
