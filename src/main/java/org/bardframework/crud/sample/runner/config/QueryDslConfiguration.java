8c348eb868f020a26573cbf05a1bf77494f7c0a7
Vahid Zafari <va.zafari@gmail.com>
2022-10-07 10:40:37 +0330

    2.7.4

--- src/main/java/org/bardframework/crud/sample/runner/config/QueryDslConfiguration.java
+++ src/main/java/org/bardframework/crud/sample/runner/config/QueryDslConfiguration.java
@@ -1,7 +1,9 @@
 package org.bardframework.crud.sample.runner.config;
 
+import com.querydsl.sql.Configuration;
 import com.querydsl.sql.SQLQueryFactory;
 import com.querydsl.sql.SQLTemplates;
+import com.querydsl.sql.spring.SpringConnectionProvider;
 import com.querydsl.sql.types.JSR310LocalDateTimeType;
 import com.querydsl.sql.types.JSR310LocalDateType;
 import com.querydsl.sql.types.JSR310LocalTimeType;
@@ -9,19 +11,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.util.function.Supplier;
 
 public class QueryDslConfiguration {
 
-    private com.querydsl.sql.Configuration querydslConfiguration(@Autowired SQLTemplates templates) {
-        com.querydsl.sql.Configuration configuration = new com.querydsl.sql.Configuration(templates);
+    @Bean
+    public SQLQueryFactory queryFactory(@Autowired DataSource dataSource, @Autowired SQLTemplates sqlTemplates) {
+        Configuration configuration = new Configuration(sqlTemplates);
         configuration.register(new JSR310LocalDateTimeType());
         configuration.register(new JSR310LocalDateType());
         configuration.register(new JSR310LocalTimeType());
-        return configuration;
-    }
-
-    @Bean
-    public SQLQueryFactory queryFactory(@Autowired DataSource dataSource, @Autowired SQLTemplates templates) {
-        return new SQLQueryFactory(querydslConfiguration(templates), dataSource);
+        Supplier<Connection> provider = new SpringConnectionProvider(dataSource);
+        return new SQLQueryFactory(configuration, provider);
     }
 }
