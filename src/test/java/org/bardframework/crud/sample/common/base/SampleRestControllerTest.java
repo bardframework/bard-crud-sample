b4aef177c6ef55e965501d2ec8bc7c41dbe5366a
v.zafari <va.zafari@gmail.com>
2024-08-07 09:27:09 +0330

    remove @Slf4j

--- src/test/java/org/bardframework/crud/sample/common/base/SampleRestControllerTest.java
+++ src/test/java/org/bardframework/crud/sample/common/base/SampleRestControllerTest.java
@@ -1,25 +1,19 @@
 package org.bardframework.crud.sample.common.base;
 
+import lombok.Getter;
 import org.bardframework.crud.api.base.ServiceDataProvider;
 import org.springframework.aop.framework.AopProxyUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Getter
 public abstract class SampleRestControllerTest<L, P extends ServiceDataProvider<?, ?, ?, ?, ?, I, ?>, I> extends SampleControllerTest {
 
     @Autowired
     private P dataProvider;
     @Autowired
     private L controller;
-
-    public P getDataProvider() {
-        return dataProvider;
-    }
-
-    public L getController() {
-        return controller;
-    }
 
     public String BASE_URL() {
         return "/" + AopProxyUtils.ultimateTargetClass(this.getController()).getAnnotation(RequestMapping.class).value()[0];
